ed76edc5332522d09a927e9aeb5555bc466803ec
Skoomy <cyril.noirot@gmail.com>
2016-04-22 13:17:24 +0200

    Categorical Variable

--- Titanic_Analyse_descriptive.R
+++ Titanic_Analyse_descriptive.R
@@ -1,7 +1,7 @@
 ##Titanic Kaggle
 
-library(ggplot2)
-library(randomForest)
+library(ggplot2) #Beautifull plot
+library(randomForest) 
 library(dplyr)
 library(ISLR)
 library(stats)
@@ -15,7 +15,7 @@ library(nnet)
 library(ROCR)
 library(tree)
 library(rpart)
-library(pROC)
+library(pROC) 
 library(party)
 library(e1071)
 
@@ -24,15 +24,24 @@ titanic <- read.csv("~/ensiie_S4/projet_MOST/train.csv")
 test<-read.csv("~/ensiie_S4/projet_MOST/test.csv")
 gendermodel<-read.csv("~/ensiie_S4/projet_MOST/gendermodel.csv")
 
-test$Survived<-NA
-alldata_1 <- rbind(titanic,test)
-
+md.pattern(titanic) ## 177 missing values in Age
 dim(titanic)
+
+#Categorial variable
+
+str(titanic) #Know the type of your variables 
+
+#Transfoorm in factor
+titanic$Pclass<-as.factor(titanic$Pclass)
+titanic$Survived<-as.factor(titanic$Survived) #Survived? yes = 1 no= 0
+titanic$Embarked<-as.factor(titanic$Embarked)
+titanic$Sex<-as.factor(titanic$Sex)
+
 
 ###PieSex
 
 
-table(titanic$Sex)
+total=nrow(titanic)
 
 dfSex <- data.frame(
   group = c("Male", "Female"),
@@ -45,11 +54,65 @@ pieSex=pieSex + scale_fill_brewer("Sex",palette = "Reds")+ theme_classic()+
   theme(axis.text.x=element_blank())+ggtitle("Gender")+
   geom_text(aes(y = value/2 + c(0, cumsum(value)[-length(value)]), 
                 label = percent(value/total)), size=5)
-######################PlotSex
-plotSex=ggplot(titanic_1, aes(x=Sex, y=Survived,fill=Sex)) + geom_bar(stat="identity") +
+######################PlotGender
+plotSex=ggplot(titanic, aes(x=Sex, y=Survived,fill=Sex)) + geom_bar(stat="identity") +
   facet_grid(. ~ Survived) +scale_fill_brewer(palette="Reds")+theme_dark()+
   theme(strip.text = element_text(face="bold", size=rel(1.5)),
         strip.background = element_rect(fill="lightblue", colour="black",
                                         size=1))
 
-grid.arrange(pieSex,plotSex, nrow=2, ncol=1)
+
+grid.arrange(pieSex,plotSex, nrow=2, ncol=1) #to put them both on same page 
+
+
+
+###############Pie Embarked
+table(titanic$Embarked)
+
+dfEmbarked <- data.frame(
+  group = c("Cherbourg", "Queenstown","Southampton"),
+  value = c(168,77,646)
+)
+pieEmbarked<- ggplot(dfEmbarked, aes(x="", y=value, fill=group))+
+  geom_bar(width = 1, stat = "identity")
+pieEmbarked <- pieEmbarked + coord_polar("y", start=0)
+pieEmbarked=pieEmbarked + scale_fill_brewer("Embarked",palette = "Blues")+ theme_classic()+
+  theme(axis.text.x=element_blank())+ggtitle("Embarkation Port")+
+  geom_text(aes(y = value/3 + c(0, cumsum(value)[-length(value)]), 
+                label = percent(value/total)), size=5)
+
+#Embarked VS Survived
+plotEmbarked=ggplot(titanic, aes(x=Embarked, y=Survived,fill=Embarked)) + geom_bar(stat="identity") +
+  facet_grid(. ~ Survived) +scale_fill_brewer(palette="Blues")+theme_dark()+
+  theme(strip.text = element_text(face="bold", size=rel(1.5),colour="black"),
+        strip.background = element_rect(fill="lightblue",colour="black",
+                                        size=1))
+
+grid.arrange(pieEmbarked,plotEmbarked, nrow=2, ncol=1)
+
+
+
+#####################################PiePclass
+table(titanic$Pclass)
+
+dfPclass <- data.frame(
+  group = c("1st Class", "2nd Class","3rd Class"),
+  value = c(216,184,491)
+)
+piePclass<- ggplot(dfPclass, aes(x="", y=value ,fill=group))+
+  geom_bar(width = 1, stat = "identity")
+piePclass <- piePclass + coord_polar("y", start=0)
+piePclass=piePclass + scale_fill_brewer("Pclass",palette = "Greens")+ theme_classic()+
+  theme(axis.text.x=element_blank())+ggtitle("Passenger Class")+
+  geom_text(aes(y = value/3 + c(0, cumsum(value)[-length(value)]), 
+                label = percent(value/total)), size=5)
+
+#Survived vs Pclass
+plotPclass=ggplot(titanic, aes(x=Pclass, y=Survived,fill=Pclass))+ geom_bar(stat="identity")+
+  facet_grid(. ~ Survived) +scale_fill_brewer(palette="Greens")+theme_dark()+
+  theme(strip.text = element_text(face="bold", size=rel(1.5)),
+        strip.background = element_rect(fill="lightblue", colour="black",
+                                        size=1))
+piePclass
+plotPclass
+grid.arrange(piePclass,plotPclass, nrow=2, ncol=1)
