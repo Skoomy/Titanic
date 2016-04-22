298594c286c83b697aa3d59ffb1605d4721be79c
Skoomy <cyril.noirot@gmail.com>
2016-04-22 12:18:14 +0200

    Pie Gender

--- Titanic_Analyse_descriptive.R
+++ Titanic_Analyse_descriptive.R
@@ -26,3 +26,30 @@ gendermodel<-read.csv("~/ensiie_S4/projet_MOST/gendermodel.csv")
 
 test$Survived<-NA
 alldata_1 <- rbind(titanic,test)
+
+dim(titanic)
+
+###PieSex
+
+
+table(titanic$Sex)
+
+dfSex <- data.frame(
+  group = c("Male", "Female"),
+  value = c(314, 577)
+)
+pieSex<- ggplot(dfSex, aes(x="", y=value, fill=group))+
+  geom_bar(width = 1, stat = "identity")
+pieSex <- pieSex + coord_polar("y", start=0)
+pieSex=pieSex + scale_fill_brewer("Sex",palette = "Reds")+ theme_classic()+
+  theme(axis.text.x=element_blank())+ggtitle("Gender")+
+  geom_text(aes(y = value/2 + c(0, cumsum(value)[-length(value)]), 
+                label = percent(value/total)), size=5)
+######################PlotSex
+plotSex=ggplot(titanic_1, aes(x=Sex, y=Survived,fill=Sex)) + geom_bar(stat="identity") +
+  facet_grid(. ~ Survived) +scale_fill_brewer(palette="Reds")+theme_dark()+
+  theme(strip.text = element_text(face="bold", size=rel(1.5)),
+        strip.background = element_rect(fill="lightblue", colour="black",
+                                        size=1))
+
+grid.arrange(pieSex,plotSex, nrow=2, ncol=1)
